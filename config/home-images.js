ac2649aafdfc0e9148dacdd92926b5130609e581
brendan-holmes <brendan.holmes@xero.com>
2022-04-09 19:48:14 +1000

    added image for home page

---
+++ config/home-images.js
@@ -0,0 +1,7 @@
+const images = [
+    {
+        src: '/w/3456/q/75/i/09f18646265802865c914785c7a421112f317cbd763cd45fca71171b039d9514/IMG_3777.JPG',
+    }
+]
+
+export default images
--- pages/home.js
+++ pages/home.js
@@ -1,15 +1,36 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
+import Image from 'next/image'
+import images from '../config/home-images'
+import imageLoader from '../components/imageLoader'
 
 export default function Index() {
-  return (
-    <Layout>
-      <div>
-        <Head>
-          <title>stelo</title>
-        </Head>
-        <p>[Photo placeholder]</p>
-      </div>
-    </Layout>
-  )
+  if (images && Array.isArray(images) && images.length >= 1) {
+    return (
+      <Layout>
+        <div>
+          <Head>
+            <title>stelo</title>
+          </Head>
+          <div className='home-image-container'>
+              {images.map(
+                image => (
+                  <Image 
+                    key={image.src}
+                    alt="about-image"
+                    src={image.src} 
+                    width={500}
+                    height={809}
+                    loader={imageLoader} 
+                    />
+                )
+              )}
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+  else {
+    return null;
+  }
 }
